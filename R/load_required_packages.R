c2526436e07cf1f8e3b99f26ab9588695422d1f3
Edward Nelson Kankaka <ken1edd@gmail.com>
2025-07-01 17:54:20 +0300

    Update all files with latest changes

--- R/load_required_packages.R
+++ R/load_required_packages.R
@@ -3,10 +3,6 @@ library(Biostrings)
 library(ape)
 library(phangorn)
 library(pegas)
-library(Biostrings)
 library(strex) # for str_elem
 library(rjags)
 library(tidyverse)
-
-#' Loads all required packages at once
-#' 
